57d15e34ac28e4cffd32579a87036b025d16d251
Nishant Sabbarwal <54791056+Nishant3007@users.noreply.github.com>
2020-10-18 15:47:08 +0530

    Update fibbonaci.cpp

--- C++/fibbonacci/fibbonaci.cpp
+++ C++/fibbonacci/fibbonaci.cpp
@@ -1,25 +1,29 @@
+//Fibonnaci series using recursion
+//Author: Nishant Sabbarwal
+
 #include<iostream>    
 using namespace std;      
 void Fibonacci(int n)
 {    
-    static int num1=0, num2=1, num3;    
+    static int no1=0, no2=1, no3;    
     if(n>0)
 	{    
-         num3 = num1 + num2;    
-         num1 = num2;    
-         num2 = num3;    
-         cout<<num3<<" ";    
+         no3 = no1 + no2;    
+         no1 = no2;    
+         no2 = no3;    
+         cout<<no3<<" ";    
          Fibonacci(n-1);    
     }    
 }    
 int main()
 {    
     int n;    
-    cout<<"Please Enter the number of elements you want to print : ";    
+    cout<<"Enter the number of elements: ";    
     cin>>n;    
     cout<<"Series are : ";    
     cout<<"0 "<<"1 ";  
     Fibonacci(n-2);   
      return 0;  
 } 
+
 
