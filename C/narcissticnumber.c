eb793a1d86738a1e2c26b6683bbd791070c2bfaf
Uthsav Jain <85090938+uthsavj@users.noreply.github.com>
2021-10-26 15:51:03 +0530

    Create narcissticnumber.c
    An n-digit number that is the sum of the nth powers of its digits is called a n-narcissistic number.

---
+++ C/narcissticnumber.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <math.h>
+int fun()
+{
+    int num, count = 0, a;
+    scanf("%d", &num);
+    a = num;
+    while (a > 0)
+    {
+        a /= 10;
+        count++;
+    }
+    int arr[count], b, sum = 0;
+    b = num;
+    for (int i = 0; i < count; i++)
+    {
+        arr[i] = b % 10;
+        b /= 10;
+        sum += pow(arr[i], count);
+    }
+    int out;
+    if (num == sum)
+    {
+        out = 1;
+    }
+    else
+    {
+        out = 0;
+    }
+    return out;
+}
+int main()
+{
+    int input;
+    scanf("%d", &input);
+    int arr[input];
+    for (int i = 0; i < input; i++)
+    {
+        arr[i] = fun();
+    }
+    for (int i = 0; i < input; i++)
+    {
+        if (arr[i] == 1)
+        {
+            printf("Yes\n");
+        }
+        else
+        {
+            printf("No\n");
+        }
+    }
+
+    return 0;
+}
