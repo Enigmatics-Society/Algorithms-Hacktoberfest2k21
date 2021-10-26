be4979540037f15d41a124a01a9dd5272a946301
theMillenniumFalcon <nishankpr2002@gmail.com>
2021-10-27 03:38:51 +0530

    java_algorithms_added

---
+++ Java/Soundex Implementation/Soundex.java
@@ -0,0 +1,69 @@
+public class Soundex 
+{
+    public static String getGode(String s) 
+    {
+        char[] x = s.toUpperCase().toCharArray();
+                 
+        char firstLetter = x[0];
+ 
+        for (int i = 0; i < x.length; i++) {
+            switch (x[i]) {
+            case 'B':
+            case 'F':
+            case 'P':
+            case 'V': {
+                x[i] = '1';
+                break;
+            }
+ 
+            case 'C':
+            case 'G':
+            case 'J':
+            case 'K':
+            case 'Q':
+            case 'S':
+            case 'X':
+            case 'Z': {
+                x[i] = '2';
+                break;
+            }
+ 
+            case 'D':
+            case 'T': {
+                x[i] = '3';
+                break;
+            }
+ 
+            case 'L': {
+                x[i] = '4';
+                break;
+            }
+ 
+            case 'M':
+            case 'N': {
+                x[i] = '5';
+                break;
+            }
+ 
+            case 'R': {
+                x[i] = '6';
+                break;
+            }
+ 
+            default: {
+                x[i] = '0';
+                break;
+            }
+            }
+        }
+ 
+        String output = "" + firstLetter;
+
+        for (int i = 1; i < x.length; i++)
+            if (x[i] != x[i - 1] && x[i] != '0')
+                output += x[i];
+
+        output = output + "0000";
+        return output.substring(0, 4);
+    }
+}
